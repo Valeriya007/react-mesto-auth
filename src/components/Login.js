e5292464ab47e0a6bbfcaba34b079d5be2c42cff
Valeriya Ivanova <valeriyaivanovalera@mail.ru>
2023-08-20 01:05:33 +0300

    work 12

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ export default function Login({ onLogin }) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        onLogin({ email, password });
+        onLogin(email, password);
 
     }
 
